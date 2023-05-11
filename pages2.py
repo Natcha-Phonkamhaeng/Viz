e3de50b0e0d7e0717c06d80b22be1603657f078a
Natcha-Phonkamhaeng <98969416+Natcha-Phonkamhaeng@users.noreply.github.com>
2023-05-11 23:01:39 +0700

    Add files via upload

--- pages2.py
+++ pages2.py
@@ -8,61 +8,44 @@ import plotly.express as px
 
 dash.register_page(__name__, path='/page-2', name='Container Deposit')
 
-df = px.data.tips()
-fig = px.sunburst(df, path=['day', 'time', 'sex'], values='total_bill')
-
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+df = df[['continent', 'country', 'pop', 'lifeExp']]
 dff = px.data.gapminder()
+
+dfTips = px.data.tips()
+fig = px.sunburst(dfTips, path=['day', 'time', 'sex'], values='total_bill')
 fig1 = px.scatter(dff, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
            size="pop", color="continent", hover_name="country",
            log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
 
 
-data = OrderedDict(
-    [
-        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
-        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
-        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
-        ("Humidity", [10, 20, 30, 40, 50, 60]),
-        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
-    ]
-)
-
-df = pd.DataFrame(data)
-
-
 layout = dbc.Container([
-	dbc.Row([
-		dbc.Col([
-			dcc.Dropdown(["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"], id='dropdown-year', multi=True, placeholder='select city')
-			], width={'size': 4}),
-		dbc.Col([
-			dcc.Dropdown(["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"], 
-    		id='dropdown-month', multi=True, placeholder='select day')
-			], width={'size':4}),
-		]),
-	html.Br(),
-	dbc.Row([
-		dbc.Col([
-			dash_table.DataTable(
-				id='table-container',
-			    data=df.to_dict('records'),
-			    columns=[{'id': c, 'name': c} for c in df.columns],
-			    style_as_list_view=True,
-			    style_data={ 'border': '1px solid black' },
-			    style_header={
-			        'backgroundColor': '#f8f9fa',
-			        'fontWeight': 'bold'
-			    },
-			    style_cell_conditional=[
-			        {
-			            'if': {'column_id': c},
-			            'textAlign': 'left'
-			        } for c in ['Date', 'Region']
-			    ]
-			)	
-			], width={'size':11})
-		]),
-	dbc.Row([
+    dcc.Markdown('# Interactive Container Deposit', style={'textAlign':'center'}),
+    dbc.Row([
+    	dbc.Col([
+    		continent_drop := dcc.Dropdown([x for x in sorted(df.continent.unique())], placeholder='select continent')
+    		], width=3),
+    	dbc.Col([
+    		country_drop := dcc.Dropdown([x for x in sorted(df.country.unique())], placeholder='select country')
+    		], width=3)
+    	]),
+    html.Br(),
+    dbc.Row([
+    	dbc.Col([
+    		my_table := dash_table.DataTable(
+    			columns=[
+		            {'name': 'Continent', 'id': 'continent', 'type': 'text'},
+		            {'name': 'Country', 'id': 'country', 'type': 'text'},
+		            {'name': 'Population', 'id': 'pop', 'type': 'numeric'},
+		            {'name': 'Life Expectancy', 'id': 'lifeExp', 'type': 'numeric'}
+		        ],
+		        data=df.to_dict('records'),
+		        page_size=5,
+		        filter_action='native'
+    			)
+    		])
+    	]),
+    dbc.Row([
 		dbc.Col([
 			dcc.Graph(figure=fig)
 			], width={'size': 5}),
@@ -70,19 +53,22 @@ layout = dbc.Container([
 			dcc.Graph(figure=fig1)
 			],width={'size':7})
 		])
-	])
+    ])
+    
+@callback(
+	Output(my_table, 'data'),
+	Input(continent_drop, 'value'),
+	Input(country_drop, 'value')
+	)
+def update_dash(value_ctn, value_cty):
+	dff = df.copy()
 
+	if value_ctn:
+		dff = dff[dff.continent == value_ctn]
+	if value_cty:
+		dff = dff[dff.country == value_cty]
 
-
-
-
-
-
-
-
-
-
-
+	return dff.to_dict('records')
 
 
 
