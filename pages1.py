703b29d7615ed30833042811098ae906e46964e3
Natcha-Phonkamhaeng <98969416+Natcha-Phonkamhaeng@users.noreply.github.com>
2023-05-17 23:56:12 +0700

    Add files via upload

--- pages1.py
+++ pages1.py
@@ -1,83 +1,71 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import Dash, html, Output, Input, dcc, callback
+from dash import Dash, html, Output, Input, dcc, callback, dash_table
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 
-dash.register_page(__name__, path='/page-1', name='Dishonoured Cheque')
+dash.register_page(__name__, path='/', name='Dishonoured Cheque')
 
-chqCounts = pd.DataFrame(dict(
-    month = ['Apr22', 'May22', 'Jun22', 'Jul22', 'Aug22','Sep22', 
-    		'Oct22', 'Nov22', 'Dec22','Jan23', 'Feb23', 'Mar23', 'Apr23'],
-    counts = [4,1,4,2,1,1,1,1,0,4,1,0,1]
-))
+df = pd.read_excel('chq.xlsx')
+print(df.head(1))
 
-df_ = pd.read_excel('chq.xlsx')
-df = df_.copy()
-fig = px.line(chqCounts, x="month", y="counts",markers=True)
-fig.update_layout(xaxis_title='', yaxis_title='number of dishonoured cheque')
+months = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar']
 
-CARD_STYLE = {
-	"background-color": "#e1eef0"
-}
+fig = go.Figure(data=[
+	go.Bar(name='FY22', x=months, y=[4,1,4,2,1,1,1,1,0,4,1,0]),
+	go.Bar(name='FY23', x=months, y=[1]),
+	])
+fig.update_layout(barmode='group', title='Volume of dishonoured cheque by year',yaxis={'title': 'No of dishonoured cheque'})
+
+bound = pd.DataFrame.from_dict({
+	'Year': ['FY22', 'FY22', 'FY22', 'FY22', 'FY22', 'FY22', 'FY22', 'FY22','FY22', 'FY22', 'FY22', 'FY22', 'FY22','FY23'],
+	'Month': ['Apr', 'Apr', 'May', 'Jun','Jul','Jul','Aug','Sep', 'Oct', 'Nov', 'Jan','Jan', 'Feb', 'Apr'],
+	'Bound': ['OB', 'IB', 'OB', 'OB', 'OB', 'IB', 'OB', 'IB','OB', 'OB', 'OB', 'IB', 'OB','OB'],
+	'Volume': [2,2,1,4,1,1,1,1,1,1,2,2,1,1],
+	})
+
+
+fig1 = px.sunburst(bound, path=['Year', 'Month', 'Bound'], values='Volume', color='Month', title='Bound by month')
 
 
 layout = dbc.Container([
 	dbc.Row([
 		dbc.Col([
 			dcc.Markdown('## Dishonoured Cheque As of Apr 22 - Apr 23', style={'color':'#160c9c', 'textAlign':'center'})
-			]),
-		html.Br(),
-		]),
-	dbc.Row([
-		dbc.Col([
-			dcc.Dropdown(['Overview', 'Monthly Detail'], placeholder='Overview')
 			])
 		]),
 	dbc.Row([
 		dbc.Col([
-				dcc.Graph(id='graph-fig', figure=fig)
-			])
+			dcc.Graph(id='graph-fig', figure=fig)
+			], width=8),
+		dbc.Col([
+			dcc.Graph(id='pie-fig', figure=fig1)
+			], width=4)
 		]),
 	dbc.Row([
 		dbc.Col([
-			dbc.Card([
-				dbc.CardBody([
-					html.H4('Total'),
-					html.H1(f'{df.shape[0]}'),
-					html.H6('of dishonoured cheque')
-					])
-				],style=CARD_STYLE)
-			]),
+			year_drop:= dcc.Dropdown([x for x in df['Year'].unique()], placeholder='select year')
+			], width=3),
 		dbc.Col([
-			dbc.Card([
-				dbc.CardBody([
-					html.H4('Most Reason'),
-					html.H1(f'{df.value_counts("Reason").reset_index()["Reason"][0]}'),
-					html.H6('Drawer\'s signature')
-					])
-				],style=CARD_STYLE)
-			]),
+			month_drop:= dcc.Dropdown([x for x in df['Month'].unique()], placeholder='select month')
+			], width=3)
+		]),
+	dbc.Row([
 		dbc.Col([
-			dbc.Card([
-				dbc.CardBody([
-					html.H4('Most is Outbound'),
-					html.H1(15),
-					html.H6('While Inbound is 6')
-					])
-				],style=CARD_STYLE)
-			]),
-		dbc.Col([
-			dbc.Card([
-				dbc.CardBody([
-					html.H4('Max Amount'),
-					html.H1('910,610.47'),
-					html.H6('By Thai Fresh Products')
-					])
-				],style=CARD_STYLE)
+			my_table := dash_table.DataTable(
+		        data=df.to_dict('records'),
+		        columns=[
+		        {'name': 'Year', 'id': 'Year'},
+		        {'name': 'Month', 'id': 'Month'},
+		        {'name': 'Company', 'id': 'Company'},
+		        {'name': 'Amount', 'id': 'Amount'}
+		        ]
+		       )
 			])
 		])
 	])
-		
+	
 
 
