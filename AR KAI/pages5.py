b72729472c4517683849b14ea02c86d88cc2f1c8
Natcha-Phonkamhaeng <98969416+Natcha-Phonkamhaeng@users.noreply.github.com>
2023-06-20 16:29:09 +0700

    Add files via upload

--- AR KAI/pages5.py
+++ AR KAI/pages5.py
@@ -21,9 +21,21 @@ df_overdue_kpi = pd.DataFrame.from_dict({
     'Criteria': ['>23%', '16.01 - 23.00%', '15.01% - 16.00%', '14.01% - 15.00%', '0% - 14.00%']                 
 })
 
+df60Days = pd.DataFrame.from_dict({
+    'Month': ['Apr', 'May'],
+    'Result': [0.0004, 0.0006],
+})
+
+dfOverDue = pd.DataFrame.from_dict({
+    'Month': ['Apr', 'May'],
+    'Result': [0.18, 0.25],
+})
+
+
 ar_guage = go.Figure(go.Indicator(
     domain = {'x': [0, 1], 'y': [0, 1]},
     value = 0.06, # this value will be change according to AR Dashboard
+    number= {'suffix': '%'},
     mode = "gauge+number",
     title = {'text': "AR >60 days KPI : Exceeds Expectations", 'font': {'color':'green', 'size': 28}},
     gauge = {'axis': {'range': [None, 2]},
@@ -40,6 +52,7 @@ ar_guage.update_layout(margin=dict(l=0, r=0, t=0, b=0),font={'color':'green'},pa
 overdue_guage = go.Figure(go.Indicator(
     domain = {'x': [0, 1], 'y': [0, 1]},
     value = 25, # this value will be change according to AR Dashboard
+    number= {'suffix': '%'},
     mode = "gauge+number",
     title = {'text': "Overdue Ratio KPI : Unsatisfactory", 'font': {'color':'red', 'size': 30}},
     gauge = {'axis': {'range': [None, 25]},
@@ -53,6 +66,24 @@ overdue_guage = go.Figure(go.Indicator(
                  ],
              'threshold' : {'line': {'color': "blue", 'width': 4}, 'thickness': 0.75, 'value': 23}}))
 overdue_guage.update_layout(margin=dict(l=0, r=0, t=0, b=0), font={'color':'red'},paper_bgcolor = "lavender")
+
+fig60Days = go.Figure(data=go.Scatter(x=df60Days['Month'].tolist(), y=df60Days['Result'].tolist()))
+fig60Days.update_layout(yaxis={'tickformat':".2%"}, 
+		yaxis_range=[0,0.0010], 
+		title='AR > 60 days VS total outstanding',
+		title_font_color='blue',
+		title_font_size=30,
+		paper_bgcolor='lavender',
+		plot_bgcolor="rgba(0,0,0,0)")
+
+figOverDue = go.Figure(data=go.Scatter(x=dfOverDue['Month'].tolist(), y=dfOverDue['Result'].tolist()))
+figOverDue.update_layout(yaxis={'tickformat':".2%"}, 
+		yaxis_range=[0,0.30], 
+		title='Overdue Ratio (Overdue VS Total Outstanding)',
+		title_font_color='blue',
+		title_font_size=30,
+		paper_bgcolor='lavender',
+		plot_bgcolor="rgba(0,0,0,0)")
 
 layout = dbc.Container([
 	html.Label(['Remark: Aging as at 01 May 2023'], className='mb-4'),
@@ -72,7 +103,7 @@ layout = dbc.Container([
 			]),
 		dbc.Col([
 			dbc.CardHeader([
-				html.H3(['Overdue Ratio (Overdue VS Total Outstanding'])
+				html.H3(['Overdue Ratio (Overdue VS Total Outstanding)'])
 				]),	
 			dag.AgGrid(
 				rowData= df_overdue_kpi.to_dict('records'),
@@ -91,11 +122,29 @@ layout = dbc.Container([
 					dcc.Graph(figure=ar_guage)
 					], class_name='shadow')
 				], style={'background-color': 'lavender'})
-			]),
+			], width=6),
 		dbc.Col([
 			dbc.Card([
 				dbc.CardBody([
 					dcc.Graph(figure=overdue_guage)
+					], class_name='shadow')
+				], style={'background-color': 'lavender'})
+			], width=6)
+		], class_name='mb-5'),
+	dbc.Row([
+		dbc.Col([
+			dbc.Card([
+				dbc.CardBody([
+					dcc.Graph(figure=fig60Days),
+					], class_name='shadow')
+				], style={'background-color': 'lavender'})
+			])
+		], class_name='mb-5'),
+	dbc.Row([
+		dbc.Col([
+			dbc.Card([
+				dbc.CardBody([
+					dcc.Graph(figure=figOverDue)
 					], class_name='shadow')
 				], style={'background-color': 'lavender'})
 			])
